7b78555c0baba603cc12ec5c2c03120f43506a80
Muhammad Rashed <rashedjaman768@gmail.com>
2025-09-06 19:01:06 +0600

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import Container from "../Reusable/Container/Container";
 const Home = () => {
   return (
     <Container className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">Welcome to Reusable Components</h1>
+      <h1 className="text-3xl font-bold"> Welcome  to Reusable Components</h1>
     </Container>
   );
 };
